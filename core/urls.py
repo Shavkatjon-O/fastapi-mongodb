dc557be0a90f1fd3a010968108c48eea2683dd98
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-06-29 23:18:56 +0500

    feat: home page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("", home),
 ]
