2b37c7a3293eef0aaadbe865998c460a4e1aaf37
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-06-29 23:48:08 +0500

    fix: urls

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,22 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
 
-
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, "home.html")
-
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.i18n import set_language
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path("", home),
+    path("rosetta/", include("rosetta.urls")),
+    path("set_language/", set_language, name="set_language"),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
+
+urlpatterns += i18n_patterns()
+
+if settings.DEBUG:
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
